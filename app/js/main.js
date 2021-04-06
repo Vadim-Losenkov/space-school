725af1ea66a8a56ca5fb00d32b3f88e0803c70b5
Vadim Losenkov <vprog0729@gmail.com>
2021-04-06 14:12:29 +0300

    start adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,20 @@
 $(function() {
   
+  $('.testimonials__slider').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1500
+  })
   
-  
-  
+  $('.show-image').magnificPopup({
+    type: 'inline',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+        beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr('data-effect');
+        }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
 })
